080038667016762801592b558968febc34febf20
Daniel Mendieta <damendieta@gmail.com>
2018-07-12 10:42:23 -0500

    [ADD] Nuevo módulo para calcular el historial de precios de un producto en base a las líneas de facturas de compras.

---
+++ product_purchase_valuation/__openerp__.py
@@ -0,0 +1,18 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    "name" : "Product purchase price history",
+    "version" : "9.0.0.1",
+    "depends" : [
+        'account',
+        'product',
+    ],
+    "category" : "Account",
+
+    "init_xml" : [],
+    "demo_xml" : [],
+    "data" : [
+        'views/product.xml'
+    ],
+    "active": False,
+    "installable": True
+}
